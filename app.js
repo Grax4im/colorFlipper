535ed9acc6856b2bc6241b676c5f7b916aaa8712
Robert Silveira <42106602+Grax4im@users.noreply.github.com>
2025-06-29 00:17:17 +0000

    more colors

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [
+const coresCSS = [
+    // Cores básicas
+    "red", "green", "blue", "yellow", "cyan", "magenta",
+    "black", "white", "gray", "silver", "purple", "orange",
+    
+    // Tons de vermelho/rosa
+    "darkred", "firebrick", "crimson", "indianred", "lightcoral",
+    "salmon", "darksalmon", "lightsalmon", "pink", "lightpink",
+    "hotpink", "deeppink", "palevioletred", "mediumvioletred",
+    
+    // Tons de verde
+    "darkgreen", "forestgreen", "seagreen", "mediumseagreen",
+    "lightgreen", "limegreen", "lime", "olive", "darkolivegreen",
+    "olivedrab", "yellowgreen", "lawngreen", "chartreuse",
+    
+    // Tons de azul
+    "darkblue", "mediumblue", "navy", "midnightblue", "royalblue",
+    "dodgerblue", "cornflowerblue", "skyblue", "lightskyblue",
+    "steelblue", "lightsteelblue", "aqua", "turquoise",
+    "mediumturquoise", "darkturquoise", "lightcyan",
+    
+    // Tons de amarelo/laranja
+    "gold", "lightyellow", "lemonchiffon", "lightgoldenrodyellow",
+    "papayawhip", "moccasin", "peachpuff", "palegoldenrod",
+    "khaki", "darkkhaki", "coral", "tomato", "orangered",
+    
+    // Tons de marrom/bege
+    "brown", "maroon", "saddlebrown", "sienna", "chocolate",
+    "peru", "sandybrown", "burlywood", "tan", "wheat",
+    "bisque", "blanchedalmond", "cornsilk", "antiquewhite",
+    
+    // Tons de roxo/lavanda
+    "indigo", "darkmagenta", "darkviolet", "blueviolet",
+    "mediumorchid", "mediumpurple", "rebeccapurple",
+    "thistle", "plum", "violet", "orchid", "lavender",
+    
+    // Outras cores nomeadas
+    "gainsboro", "whitesmoke", "ghostwhite", "snow",
+    "honeydew", "mintcream", "azure", "aliceblue",
+    "lavenderblush", "mistyrose", "seashell", "oldlace",
+    "linen", "floralwhite", "ivory", "beige"
+];
+
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
